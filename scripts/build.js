1dd25839eacd6c9088d6f531e227217beb58806d
Joey Nguyen <jnn193@gmail.com>
2017-05-21 18:22:27 -0500

    Changed some color settings

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@ const pupa = require('pupa')
 const readFile = require('fs').readFileSync
 const writeFile = require('fs').writeFileSync
 
-const green = ( alpha = '' ) => `b5bd68${alpha}`
+const yellow = ( alpha = '' ) => `b5bd68${alpha}`
 const lightGray = ( alpha = '' ) => `abb2bf${alpha}`
 const cyan = ( alpha = '' ) => `56b6c2${alpha}`
 const orange = ( alpha = '' ) => `d19a66${alpha}`
@@ -11,25 +11,25 @@ const purple = 'c678dd'
 const pink = 'f92672' // This is base for `storage` but gets overriden
 const red = 'df6a73'
 const white = 'f8f8f0'
-const rustGreen = '98c379'
+const green = '98c379'
 const teal = '56b6c2'
 const orient = '025a7a'
+const gray = '282c34'
 
 const base = {
   themeLabel: 'One Dark Hybrid',
   settings: {
-    background: '242e33',
+    background: gray,
     invisibles: lightGray('26'),
     lineHighlight: orient,
     selection: orient,
   },
   markdown: {
-    quote: green('88'),
-    orderedList: green('aa'),
-    unorderedList: green('dd'),
+    quote: yellow('88'),
+    orderedList: yellow('aa'),
+    unorderedList: yellow('dd'),
   },
-  grayBlue: 'abb2bf',
-  green: green(),
+  yellow: yellow(),
   cyan: cyan(),
   lightGray: lightGray(),
   lightGray2: lightGray('bb'), // String template variables
@@ -41,7 +41,7 @@ const base = {
   red,
   teal,
   white,
-  yellow: rustGreen, // Using value of 'rust green' instead of yellow
+  green,
 }
 
 const build = () => {
